1ba47d7317acca60c6596a2c96757da77c66040d
Arian Farzad <arian.farzad@stud.h-da.de>
2024-11-09 13:34:40 +0000

    Resolve "rpc-api"

---
+++ src/server.py
@@ -0,0 +1,11 @@
+"""
+Registers the `get_route` function as an XML-RPC method and starts the server 
+to listen for incoming requests.
+"""
+
+from xmlrpc.server import SimpleXMLRPCServer
+from dijkstra_algorithm import get_route
+
+server = SimpleXMLRPCServer(("0.0.0.0", 8000))
+server.register_function(get_route, "get_route")
+server.serve_forever()
