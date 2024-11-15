b1e290e3602825edefa0bc3a3008f826e35409c0
Târik-Cemal Atis <t.r.atis@stud.h-da.de>
2024-11-15 01:40:13 +0000

    FIX: load_cities() response_body and its processing

--- src/js/path_finder.js
+++ src/js/path_finder.js
@@ -15,8 +15,9 @@ const LEVELS = ['debug', 'info', 'warn', 'error', 'none'];
  * - 'error'  - Only error messages will be printed.
  * - 'none'   - No log messages are printed.
  */
-const LOG_LEVEL = 'info'; // Default value
+const LOG_LEVEL = 'none'; // Default value
 const TEST = LOG_LEVEL !== 'none'; // LOG_LEVEL == 'none' deactivates test()
+
 
 function Init() {
     "use strict";
@@ -101,7 +102,7 @@ function is_valid_json(string) {
  *
  * @param {string} jsonString - The JSON string to check for formatting.
  *
- * @returns {boolean} Returns `true` if the JSON string is formatted (contains newlines and indentation),
+ * @returns {boolean} `true` if the JSON string is formatted (contains newlines and indentation),
  *                    and `false` if the string is compact (single-line).
  */
 function is_formatted_json(jsonString) {
@@ -143,17 +144,20 @@ function format_json(string) {
  */
 class Path_finder {
 
-    /** TODO: write
+    // private class attributes
+    /** Current disabled option of <select> endpoint
      *
-     */
-
-    // global variables
-    /** current disabled option of <select> endpoint
-     *
+     * @type {number}
      * if set on 0; nothing is disabled
      * @default 0
      * */
     #currently_disabled = 0;
+    /** List of loaded cities
+     *
+     * @type {City[]}
+     * @default {[]}
+     */
+    #cities = [];
 
     /** Binds methods to events/actions and
      * contains further configurations for class path_finder.
@@ -163,61 +167,108 @@ class Path_finder {
         c_log('debug', '[Path_finder](constructor)');
 
         try {
-            // deactivate reload of webpage
-            document.getElementById('path_points_form').setAttribute('onsubmit', 'return false;');
-
+            // load cities for page initialization
             this.load_cities();
 
             // check completion of form with every change
             document.getElementById('path_points_form').oninput = this.check_form_completion.bind(this);
+            // initial check of form completion
             this.check_form_completion();
 
             // makes sure, the user cannot select the same city as start- and endpoint
             document.getElementById('startpoint').onchange = this.disable_endpoint.bind(this);
 
-            // process form data with a method
+            // deactivate reload of webpage
+            document.getElementById('path_points_form').setAttribute('onsubmit', 'return false;');
+
+            // process form data with a method instead of directly sending the form request
             document.getElementById('path_points_submit_button').onclick = this.process_form_data.bind(this);
         } catch (e) {
             c_log('error', '[Path_finder](constructor): ', e);
         }
     }
 
-    /***/
+    /** searches list of cities for a city with given name
+     *
+     * @param {string} name
+     *
+     * @returns {City} `City` if city exists; otherwise `null`
+     * */
+    get_city_by_name(name) {
+        "use strict";
+        c_log('debug', '[Path_finder](get_city_by_name)');
+
+        for (let city of this.#cities) {
+            if (name === city.get_name()) {
+                return city;
+            }
+        }
+        return null;
+    }
+
+    /** searches list of cities for a city with given coordinates
+     *
+     * @param {number} position_x
+     * @param {number} position_y
+     *
+     * @returns {City} `City` if city exists; otherwise `null`
+     * */
+    get_city_by_coordinates(position_x, position_y) {
+        "use strict";
+        c_log('debug', '[Path_finder](get_city_by_coordinates)');
+
+        for (let city of this.#cities) {
+            if (position_x === city.get_position_x() && position_y === city.get_position_y()) {
+                return city;
+            }
+        }
+        return null;
+    }
+
+    /** Fetches the cities from a backend api and saves them as city objects in the #cities attribute.
+     *
+     * @returns {string} -  A JSON string with only the names of the loaded cities
+     */
     load_cities() {
         "use strict";
         c_log('debug', '[Path_finder](load_cities)');
 
-        let counter = 0;
-
-        /* As long as fetching from Backend doesn't work; load json map data
+        /* TODO: fetch from Backend api
+         * As long as fetching from Backend doesn't work; load json map data
          * response structure: map{"cities": [{...}, ...], "connections": [{...}, ...], "mapname": "..." }
          */
         fetch('../../assets/json/load_cities_response_body.json')
             .then(response => response.json())
             .then(map => {
-                const endpoint_list = document.getElementById('endpoint_list');
-                const select_startpoint = document.getElementById('startpoint');
-                const select_endpoint = document.getElementById('endpoint');
+                if (map.cities !== undefined) {
 
-                for (let i = 0; map.cities[i.toString()]; i++) {
-                    let city_name = map.cities[i.toString()].toString();
+                    const endpoint_list = document.getElementById('endpoint_list');
+                    const select_startpoint = document.getElementById('startpoint');
+                    const select_endpoint = document.getElementById('endpoint');
 
-                    let list_item = document.createElement('li');
-                    list_item.innerText = city_name;
-                    endpoint_list.appendChild(list_item);
+                    let counter = 0;
 
-                    let option = document.createElement('option');
-                    option.value = (++counter).toString();
-                    option.textContent = city_name;
-                    select_startpoint.appendChild(option);
+                    for (let city of map.cities) {
+                        c_log('debug', '[Path_finder](load_cities): city: ', city.name);
+                        this.#cities.push(new City(city.name, city.position_x, city.position_y));
 
-                    option = document.createElement('option');
-                    option.value = counter.toString();
-                    option.textContent = city_name;
-                    select_endpoint.appendChild(option);
-                }
+                        let list_item = document.createElement('li');
+                        list_item.innerText = city.name;
+                        endpoint_list.appendChild(list_item);
+
+                        let option = document.createElement('option');
+                        option.value = (++counter).toString();
+                        option.textContent = city.name;
+                        select_startpoint.appendChild(option);
+
+                        option = document.createElement('option');
+                        option.value = counter.toString();
+                        option.textContent = city.name;
+                        select_endpoint.appendChild(option);
+                    }
+                } else throw 'Cities undefined';
             })
-            .catch(error => c_log('error', '[Path_finder](load_cities): Error fetching cities:', error));
+            .catch(error => c_log('error', '[Path_finder](load_cities): Error fetching cities: ', error));
     }
 
     /** Disables the city selected as startpoint for the endpoint selection
@@ -309,14 +360,14 @@ class Path_finder {
      * - Awaits the response from the service.
      * - Updates the webpage with the response data.
      *
-     * @async Due to Service communication.
+     * @async Due to Server communication.
      */
     async process_form_data() {
         "use strict";
         c_log('debug', '[Path_finder](process_form_data)');
 
         let form_data = this.retrieve_form_data();
-        c_log('info', '[Path_finder](process_form_data): ', form_data);
+        c_log('info', '[Path_finder](process_form_data): form_data: ', form_data);
 
         try {
             let response_data = await this.get_route(form_data);
@@ -336,7 +387,10 @@ class Path_finder {
      *   // or
      *   c_log('info', formData);
      * Output:
-     *   '{ "startpoint": "Start Location", "endpoint": "End Location" }'
+     *   '{
+     *      "startpoint": "Start City",
+     *      "endpoint": "End City"
+     *   }'
      *
      * @returns {string} A JSON string representing the selected `startpoint` and `endpoint` values.
      */
@@ -362,7 +416,7 @@ class Path_finder {
             }
         }
 
-        return JSON.stringify(form_data, null, 2);
+        return format_json(form_data);
     }
 
     /** Sends given form data to a remote service using the GET-method and returns its response.
@@ -391,7 +445,7 @@ class Path_finder {
         c_log('debug', '[Path_finder](get_route)');
 
         request_body = format_json(request_body);
-        c_log('info', '[Path_finder](get_route): ', request_body);
+        c_log('info', '[Path_finder](get_route): request_body: ', request_body);
 
         // As long as fetching in general doesn't work; use the request body itself as response
         let response_data = {};
@@ -403,26 +457,27 @@ class Path_finder {
         await fetch('../../assets/json/get_route_response_body.json')
             .then(response => response.json())
             .then(data => {
-                for (let i = 0; data.route[i.toString()]; i++) {
-                    let city_name = data.route[i.toString()];
+                if (data.route !== undefined) {
 
-                    // set startpoint(first) and endpoint(last)
-                    if (i === 0) {
-                        startpoint = city_name;
-                    } else {
-                        endpoint = city_name;
+                    for (let i = 0; data.route[i.toString()]; i++) {
+                        let city_name = data.route[i.toString()];
+
+                        // set startpoint(first) and endpoint(last)
+                        if (i === 0) {
+                            startpoint = city_name;
+                        } else {
+                            endpoint = city_name;
+                        }
+                        route += city_name + ' => ';
                     }
+                    route = route.substring(0, (route.length - ' => '.length));
 
-                    route += city_name + ' => ';
-                }
-                route = route.substring(0, (route.length - ' => '.length));
+                    response_data.route = 'Route from ' + startpoint + ' to ' + endpoint + ': ' + route + '.';
 
-                response_data.route = 'Route from ' + startpoint + ' to ' + endpoint + ': ' + route + '.';
-
-                response_data.distance = 'The distance is ' + (parseFloat(data.distance).toFixed(2)).toString() + ' [units of length].';
-
+                    response_data.distance = 'The distance is ' + (parseFloat(data.distance).toFixed(2)).toString() + ' [units of length].';
+                } else throw 'Route undefined'
             })
-            .catch(error => c_log('error', '[Path_finder](get_route): Error:', error));
+            .catch(error => c_log('error', '[Path_finder](get_route): Error fetching route: ', error));
 
         // https://echo.fbi.h-da.de/
         // https://api.group2.proxy.devops-pse.users.h-da.cloud
@@ -447,7 +502,7 @@ class Path_finder {
     /** Updates the HTML response section with the provided data.
      *
      * If the response section does not exist, it creates the section,
-     * appends it to the body and displays the response data inside it.
+     * appends it into the main tag and displays the response data inside it.
      *
      * @param {string} data - The data to be displayed in the response section.
      */
@@ -490,8 +545,110 @@ class Path_finder {
 
             article.appendChild(distance_p);
         } else {
-            document.getElementById('route_p').innerHTML = response_data.route;
-            document.getElementById('distance_p').innerHTML = response_data.distance;
+            document.getElementById('route_p').innerText = response_data.route;
+            document.getElementById('distance_p').innerText = response_data.distance;
         }
+    }
+}
+
+/** A class to save city information for easier handling
+ * It handles following tasks:
+ * - saving city information
+ * - TODO: list tasks when class starts to be used
+ */
+class City {
+
+    // private class attributes
+    /** Name of the city
+     * @default ''
+     */
+    #name;
+    /** X coordinate of the city
+     * @default 0
+     */
+    #position_x;
+    /** Y coordinate of the city
+     * @default 0
+     */
+    #position_y;
+
+    /** Fills class attributes with given parameters
+     *
+     * @param {string} name
+     * @param {number} position_x
+     * @param {number} position_y
+     *
+     */
+    constructor(name, position_x, position_y) {
+        "use strict";
+        c_log('debug', '[Path_finder](constructor)');
+
+        this.#name = name;
+        this.#position_x = position_x;
+        this.#position_y = position_y;
+    }
+
+    /** Getter for private class attribute name
+     * @returns {string} name
+     */
+    get_name() {
+        "use strict";
+        c_log('debug', '[Path_finder](get_name)');
+
+        return this.#name;
+    }
+
+    /** Getter for private class attribute position_x
+     * @returns {number} position_x
+     */
+    get_position_x() {
+        "use strict";
+        c_log('debug', '[Path_finder](get_position_x)');
+
+        return this.#position_x;
+    }
+
+    /** Getter for private class attribute position_y
+     * @returns {number} position_y
+     */
+    get_position_y() {
+        "use strict";
+        c_log('debug', '[Path_finder](get_position_y)');
+
+        return this.#position_y;
+    }
+
+    /** Creates an unformatted JSON string of itself
+     * @returns {string} - JSON formatted
+     */
+    to_json_string() {
+        "use strict";
+        c_log('debug', '[Path_finder](to_json_string)');
+
+        return JSON.stringify(this.to_json());
+    }
+
+    /** Creates a formatted JSON string of itself
+     * @returns {string} - JSON formatted
+     */
+    to_formatted_json_string() {
+        "use strict";
+        c_log('debug', '[Path_finder](to_formatted_json_string)');
+
+        return format_json(this.to_json());
+    }
+
+    /** Creates a JSON object of itself
+     * @returns {Object} - JSON object
+     */
+    to_json() {
+        "use strict";
+        c_log('debug', '[Path_finder](to_json)');
+
+        let self = {};
+        self.name = this.#name;
+        self.position_x = parseInt(this.#position_x);
+        self.position_y = parseInt(this.#position_y);
+        return JSON.parse(JSON.stringify(self));
     }
 }
